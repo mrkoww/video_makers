02e8631f37df02d0b26bb322df23ed90bf28aafd
mr-koww <mr-koww@yandex.ru>
2015-08-25 14:26:12 +0500

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Helper
+  def is_maker?
+    is_maker
+  end
+end
