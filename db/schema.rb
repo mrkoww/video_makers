a8dac1810d03c08b564c4fc483f9650ef8e45c64
mr-koww <mr-koww@yandex.ru>
2015-08-25 14:55:37 +0500

    add video model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825093037) do
+ActiveRecord::Schema.define(version: 20150825095029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,17 @@ ActiveRecord::Schema.define(version: 20150825093037) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "videos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "attachment_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+  end
+
+  add_index "videos", ["attachment_id"], name: "index_videos_on_attachment_id", using: :btree
+  add_index "videos", ["user_id"], name: "index_videos_on_user_id", using: :btree
+
+  add_foreign_key "videos", "attachments"
 end
