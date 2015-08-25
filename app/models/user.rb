e7610acdafdc7f1c1e6ece84451b55f77590f55d
mr-koww <mr-koww@yandex.ru>
2015-08-25 16:47:26 +0500

    add video ref

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :videos, dependent: :destroy
+
   # Helper
   def is_maker?
     is_maker
