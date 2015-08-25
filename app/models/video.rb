da936e7176acd37a9975e97869e32590b5528904
mr-koww <mr-koww@yandex.ru>
2015-08-25 16:47:57 +0500

    add validations, associations, uploader

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,15 @@
 class Video < ActiveRecord::Base
-  belongs_to :attachment
+  # Setup accessible attributes
+  # attr_accessible :user_id, :name, :description, :file
+
+  # Validations
+  validates :name, :description, :file, presence: true
+  validates :name, length: { in: 5..50 }
+  validates :description, length:  { in: 15..250 }
+
+  # Associations
+  belongs_to :user
+
+  # Uploader
+  mount_uploader :file, VideoUploader
 end
