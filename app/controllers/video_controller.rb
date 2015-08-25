d15c6f0c82a9b0a78140c926624339720b0cdbac
mr-koww <mr-koww@yandex.ru>
2015-08-25 16:48:57 +0500

    add methods

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,15 +1,31 @@
 class VideoController < ApplicationController
+  before_action :load_video, only: [ :destroy ]
   before_action :authenticate_user!
 
-  def new
+  def index
+    respond_with @videos = Video.where(user: current_user)
+  end
 
+  def new
+    respond_with @video = Video.new
   end
 
   def create
-
+    respond_with @video = Video.create(video_params)
   end
 
   def destroy
+    respond_with @video.destroy
+  end
 
+  private
+
+  def video_params
+    params.require(:video).permit(:name, :description, :file)
+    .merge(user_id: current_user.id)
+  end
+
+  def load_video
+    @video = Video.find(params[:id])
   end
 end
